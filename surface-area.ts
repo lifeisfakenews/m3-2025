6a6d6bcce6f519b4713b4c6f58e820c7c9a88a27
lifeisfakenews <lifeisfakenews69@gmail.com>
2025-03-01 20:50:24 +0000

    ddddf

--- surface-area.ts
+++ surface-area.ts
@@ -18,7 +18,7 @@ for (const [type, [area, floors]] of Object.entries(houses)) {
     let per_floor = area / floors;
     let side = Math.sqrt(per_floor);
     let roof_side = side / (2 * Math.cos(ROOF_ANGLE));
-    let num_exposed_walls = 4;
+    let num_exposed_walls = 3;
     let roof_type = "pitched";
     if (type === "terraced") {
         num_exposed_walls = 2;
@@ -26,16 +26,16 @@ for (const [type, [area, floors]] of Object.entries(houses)) {
     if (type === "flat") {
         roof_type = "flat";
     }
-    let surface_area = per_floor + num_exposed_walls*(side * room_height * floors) + 2*(roof_side * side);
-
+    let roof_area = 2*(roof_side * side)
+    
     if (type !== "terraced" && roof_type === "pitched") {
-        surface_area += 0.5 * (side/2) * roof_side * Math.sin(ROOF_ANGLE);
+        roof_area += 0.5 * (side/2) * roof_side * Math.sin(ROOF_ANGLE);
     };
+    let surface_area = per_floor + num_exposed_walls*(side * room_height * floors) + roof_area;
+    console.log(`${type} (roof): ${roof_area}m^2`);
     result[type] = Math.round(surface_area);
 };
 
-console.log(result);
-
 for (const [type, area] of Object.entries(result)) {
-    console.log(`${type}: ${area} m^2`);
+    console.log(`${type} (walls + roof): ${area} m^2`);
 }
--- time-temp.py
+++ time-temp.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-temps = np.array([21.1, 18.9, 17.8, 17.2, 17.2, 16.1, 18.9, 25.0, 27.8, 32.2, 33.9, 36.1, 37.2, 37.2, 37.2, 35.0, 35.0, 32.8, 32.8, 32.2, 27.8, 27.8, 27.2, 26.1])
+inside_temps = np.array([21.12, 18.92, 17.82, 17.22, 17.22, 16.29, 19.34, 25.68, 28.74, 33.35, 35.22, 37.53, 38.69, 38.67, 38.59, 36.25, 36.05, 33.62, 33.36, 32.51, 27.83, 27.82, 27.22, 26.12])
+outside_temps = np.array([21.1, 18.9, 17.8, 17.2, 17.2, 16.1, 18.9, 25.0, 27.8, 32.2, 33.9, 36.1, 37.2, 37.2, 37.2, 35.0, 35.0, 32.8, 32.8, 32.2, 27.8, 27.8, 27.2, 26])
 times = np.array(["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"])
 
-plt.plot(times, temps)
+plt.plot(times, inside_temps, label="Inside Temp")
+plt.plot(times, outside_temps, label="Outside Temp")
 plt.xlabel('Time')
 plt.ylabel('Temperature')
-plt.title('Temperature over Time')
+plt.title("Temperature over time (semi detached)")
+
+
+plt.legend()
 
 plt.show()
